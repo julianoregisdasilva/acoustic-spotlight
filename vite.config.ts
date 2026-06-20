94163bb910bcdd9d893a10c07e83d4a86e02936e
Juliano Regis da Silva <157991737+julianoregisdasilva@users.noreply.github.com>
2026-06-20 17:18:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/julianoregisdasilva/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
